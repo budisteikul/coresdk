1bd04b826fe9ba9ff55443a7101e4f5bde26ec1c
Budi <aku@budi.my.id>
2026-06-29 12:21:22 +0700

    Update page.blade.php

--- src/views/layouts/page.blade.php
+++ src/views/layouts/page.blade.php
@@ -1,11 +1,11 @@
 @extends('coresdk::layouts.app')
-@section('content')
+@section('layout')
 <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Monthly Report</div>
+                <div class="card-header">{{ $mainTitle }}</div>
                 <div class="card-body">
-
+                    @yield('content')
                 </div>
             </div>
         </div>
